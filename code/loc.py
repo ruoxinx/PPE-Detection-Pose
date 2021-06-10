87186ebe367448a9a519aee4f324588fcc3e816a
ruoxinx <76619793+ruoxinx@users.noreply.github.com>
2021-06-10 14:26:05 -0400

    Update

--- code/loc.py
+++ code/loc.py
@@ -1,12 +1,9 @@
 from upbody import loc_upbody
 from head import loc_head
 
-def main(img, json):
+if __name__ == "__main__":
+    ## load images and json files
+    img = '../examples/000124.jpg'
+    json = '../examples/000124_keypoints.json'
     loc_upbody(img, json)
     loc_head(img, json)
-
-if __name__ == "__main__":
-    ## input images and json files
-    img = '../examples/000124.jpg'
-    temp = '../examples/000124_keypoints.json'
-    main(img, temp)
